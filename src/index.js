3e2b41256dbbcba7767b2d1dd201893e22d179b4
Monika <monikahoex@gmail.com>
2019-11-16 15:08:05 -0800

    Rename App

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './styles/index.css';
+import TurkeyTimer from './components/TurkeyTimer';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<TurkeyTimer />, document.getElementById('app'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
